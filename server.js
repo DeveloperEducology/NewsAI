a829def5704b4444f2537a37d4f2ff2cb29a0a2b
vijay marka <111419184+DeveloperEducology@users.noreply.github.com>
2025-08-27 01:18:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -425,7 +425,7 @@ app.post("/api/articles", async (req, res) => {
 app.get("/api/articles", async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 20;
+    const limit = parseInt(req.query.limit) || 10;
     const skip = (page - 1) * limit;
 
     const articles = await Article.find()
